b4190868f78d1fd3925fc18a573830015b9ab0e5
akingayi <akingbayiojo@gmail.com>
2024-12-10 15:23:16 +0100

    navbar complete

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,11 +7,17 @@ import { links } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
   const normalLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
+
+  const handleCloseSideBar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
 
   return (
     <>
@@ -21,7 +27,7 @@ const Sidebar = () => {
             <div className="flex justify-between items-center">
               <Link
                 to="/"
-                onClick={() => setActiveMenu(false)}
+                onClick={handleCloseSideBar}
                 className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
               >
                 <SiShopware /> <span>Shoppy</span>
@@ -50,6 +56,7 @@ const Sidebar = () => {
                   {item.links.map((link) => (
                     <NavLink
                       to={`/${link.name}`}
+                      onClick={handleCloseSideBar}
                       key={link.name}
                       className={({ isActive }) =>
                         isActive ? activeLink : normalLink
